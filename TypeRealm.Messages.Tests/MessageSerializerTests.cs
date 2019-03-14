f1f30444aa907e7b2dca10f98e12b94d687503db
Ivan Zyranau <ewancoder@gmail.com>
2019-03-15 00:33:38 +0300

    Add test project for Messages project
    Add basic MessageSerializer tests.
    
    IndexedCollection is not tested separately because this class is
    internal to Messages assembly and serves the purpose of simplifying code
    inside MessageSerializer class. If in future IndexedCollection class
    will be exposed publicly, corresponding suit of tests will be added to
    cover it.

---
+++ TypeRealm.Messages.Tests/MessageSerializerTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using ProtoBuf;
+using Xunit;
+
+namespace TypeRealm.Messages.Tests
+{
+    public class MessageSerializerTests
+    {
+        [Fact]
+        public void ShouldSerializeAuthorizeMessage()
+        {
+            byte[] bytes;
+
+            using (var stream = new MemoryStream())
+            {
+                MessageSerializer.Write(stream, new Authorize
+                {
+                    PlayerId = "player id"
+                });
+
+                bytes = stream.ToArray();
+            }
+
+            Assert.NotEmpty(bytes);
+
+            using (var stream = new MemoryStream(bytes))
+            {
+                var message = MessageSerializer.Read(stream) as Authorize;
+
+                Assert.Equal("player id", message.PlayerId);
+            }
+        }
+
+        [Fact]
+        public void ShouldNotSerializeUnknownMessage()
+        {
+            using (var stream = new MemoryStream())
+            {
+                Assert.Throws<InvalidOperationException>(
+                    () => MessageSerializer.Write(stream, new object()));
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                Assert.Throws<InvalidOperationException>(
+                    () => MessageSerializer.Read(stream));
+            }
+        }
+
+        [Fact]
+        public void ShouldNotDeserializeUnknownFieldNumber()
+        {
+            byte[] bytes;
+
+            using (var stream = new MemoryStream())
+            {
+                Serializer.NonGeneric.SerializeWithLengthPrefix(
+                    stream, "test", PrefixStyle.Base128, 50000);
+
+                bytes = stream.ToArray();
+            }
+
+            using (var stream = new MemoryStream(bytes))
+            {
+                Assert.Throws<InvalidOperationException>(
+                    () => MessageSerializer.Read(stream));
+            }
+        }
+    }
+}
---
+++ TypeRealm.Messages.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TypeRealm.Messages.Tests")]
+[assembly: AssemblyDescription("TypeRealm Messages Tests")]
+[assembly: AssemblyProduct("TypeRealm.Messages.Tests")]
+[assembly: AssemblyCopyright("Copyright © Ivan Zyranau 2019")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
