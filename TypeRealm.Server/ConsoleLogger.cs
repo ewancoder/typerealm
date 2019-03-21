8d1a6a3eedf1347ea33efb2183a4c12af0c9826a
Ivan Zyranau <ewancoder@gmail.com>
2019-03-21 18:01:04 +0300

    Split ConsoleLogger to separate file
    ConsoleLogger was declared in the same file where ILogger interface
    resides. This leads to confusion when trying to find ConsoleLogger class
    in the codebase.

---
+++ TypeRealm.Server/ConsoleLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TypeRealm.Server
+{
+    internal sealed class ConsoleLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void Log(string message, Exception exception)
+        {
+            Console.WriteLine($"{message} {exception}");
+        }
+    }
+}
--- TypeRealm.Server/ILogger.cs
+++ TypeRealm.Server/ILogger.cs
@@ -7,17 +7,4 @@ namespace TypeRealm.Server
         void Log(string message);
         void Log(string message, Exception exception);
     }
-
-    internal sealed class ConsoleLogger : ILogger
-    {
-        public void Log(string message)
-        {
-            Console.WriteLine(message);
-        }
-
-        public void Log(string message, Exception exception)
-        {
-            Console.WriteLine($"{message} {exception}");
-        }
-    }
 }
