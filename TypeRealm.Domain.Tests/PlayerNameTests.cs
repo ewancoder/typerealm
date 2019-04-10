fb6853abfeff0885fddde1b36b154cc6c5c6823d
Ivan Zyranau <ewancoder@gmail.com>
2019-04-10 18:22:04 +0300

    Add PlayerName class and tests

---
+++ TypeRealm.Domain.Tests/PlayerNameTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xunit;
+
+namespace TypeRealm.Domain.Tests
+{
+    public class PlayerNameTests
+    {
+        [Fact]
+        public void ShouldThrowWhenNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new PlayerName(null));
+        }
+
+        [Fact]
+        public void ShouldThrowWhenEmpty()
+        {
+            Assert.Throws<ArgumentException>(() => new PlayerName(string.Empty));
+        }
+
+        [Fact]
+        public void ShouldThrowWhenLengthIsLessThan3()
+        {
+            Assert.Throws<ArgumentException>(() => new PlayerName("a"));
+            Assert.Throws<ArgumentException>(() => new PlayerName("ab"));
+        }
+
+        [Fact]
+        public void ShouldThrowWhenLengthIsMoreThan20()
+        {
+            Assert.Throws<ArgumentException>(() => new PlayerName(new string('a', 21)));
+            Assert.Throws<ArgumentException>(() => new PlayerName(new string('a', 40)));
+        }
+
+        [Fact]
+        public void ShouldCreateWhenLengthIsBetween3And20()
+        {
+            Assert.Equal("abc", new PlayerName("abc").Value);
+            Assert.Equal("abcd", new PlayerName("abcd").Value);
+            Assert.Equal(new string('a', 20), new PlayerName(new string('a', 20)).Value);
+            Assert.Equal(new string('a', 19), new PlayerName(new string('a', 19)).Value);
+        }
+    }
+}
---
+++ TypeRealm.Domain/PlayerName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TypeRealm.Domain
+{
+    public sealed class PlayerName : Primitive<string>
+    {
+        public PlayerName(string value) : base(value)
+        {
+            if (value.Length < 3)
+                throw new ArgumentException("Player name should be at least 3 characters long.");
+
+            if (value.Length > 20)
+                throw new ArgumentException("Maximum length of the player name is 20 characters.");
+        }
+    }
+}
