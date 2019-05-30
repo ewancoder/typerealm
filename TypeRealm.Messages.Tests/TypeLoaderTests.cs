44de7483046816e45a3f5073c55ffebff6068806
Ivan Zyranau <ewancoder@gmail.com>
2019-05-31 02:45:31 +0300

    Test all serializable message in TypeLoader

---
+++ TypeRealm.Messages.Tests/TypeLoaderTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using TypeRealm.Messages.Connection;
+using TypeRealm.Messages.Movement;
+using Xunit;
+
+namespace TypeRealm.Messages.Tests
+{
+    public sealed class TypeLoaderTests
+    {
+        [Fact]
+        public void ShouldLoadAllTypes()
+        {
+            var types = TypeLoader.Messages.ToList();
+            Assert.Equal(11, types.Count);
+
+            foreach (var type in new[]
+            {
+                typeof(Authorize),
+                typeof(Disconnected),
+                typeof(Heartbeat),
+                typeof(Quit),
+
+                typeof(EnterRoad),
+                typeof(Move),
+                typeof(MovementStatus),
+                typeof(MovementProgress),
+                typeof(TurnAround),
+
+                typeof(Say),
+                typeof(Status)
+            })
+            {
+                Assert.Contains(type, types);
+            }
+        }
+    }
+}
